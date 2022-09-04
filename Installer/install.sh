35c48d53b3685bfa4cd0daca3c9a4811154be711
Jacob <78283092+Dunn-Dev8@users.noreply.github.com>
2022-09-04 22:46:53 +0930

    Update install.sh

--- Installer/install.sh
+++ Installer/install.sh
@@ -1,11 +1,8 @@
 !#bin/bash
 
-echo"Adding nucleolusText Binary to the /bin/ Folder"
-sleep 3
-sudo cp ./bin/nucleolusText /bin/
+cd ..
 
-echo"Make nucleolusText apart of your Desktop"
-sleep 5
+sudo cp ./bin/nucleolusText /bin/
 
 sudo cp ./bin/nucleolus.desktop /usr/share/applications/
 
